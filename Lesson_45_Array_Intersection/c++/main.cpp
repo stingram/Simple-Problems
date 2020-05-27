f9007b6568cc4ddbb08aa377b3e8904cb4114680
unknown <steven.e.ingram@gmail.com>
2020-05-27 16:49:41 -0700

    Added c++ solution.

--- Lesson_45_Array_Intersection/c++/main.cpp
+++ Lesson_45_Array_Intersection/c++/main.cpp
@@ -1,3 +1,48 @@
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <numeric>
+#include <algorithm>
+
+class Solution
+{
+    public:
+    std::unordered_map<int, bool> build_dict(const std::vector<int>& arr)
+    {
+        std::unordered_map<int, bool> res;
+        for(int num : arr)
+        {
+            if(res.find(num) == res.end())
+            {
+                res[num] = true;
+            }
+        }
+        return res;
+    }
+    std::vector<int> array_intersection(const std::vector<int>& arr1,
+                                        const std::vector<int>& arr2)
+    {
+        std::unordered_map<int, bool> set_dict;
+        std::unordered_map<int, bool> a_dict = Solution::build_dict(arr1);
+        std::vector<int> result;
+        for(int num : arr2)
+        {
+            if(a_dict.find(num) != a_dict.end() && set_dict.find(num) == set_dict.end())
+            {
+                set_dict[num] = true;
+            }
+        }
+
+        for(auto pair: set_dict)
+        {
+            int val = pair.first;
+            result.push_back(val);
+        }
+        return result;
+    }
+
+};
 
 
 
@@ -24,3 +69,19 @@
 // a1 = [4,9,5,9]
 // a2 = [9,4,9,8,4]
 // print(Solution().array_intersection(a1, a2))
+
+
+int main()
+{
+
+    std::vector<int> a1 = {4,9,5,9};
+    std::vector<int> a2 = {9,4,9,8,4};
+    auto res = Solution().array_intersection(a1,a2);
+    for(int n: res)
+    {
+        std::cout << n << " ";
+    }
+
+
+    return 0;
+}
