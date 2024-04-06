f6c4d2586a220fa17769a1a6774bbd42a70812a7
stingram <steven.e.ingram@gmail.com>
2024-04-06 01:43:20 -0500

    tp light

---
+++ hackerrank/lightpoles/lightpoles.py
@@ -0,0 +1,17 @@
+# There's a one-dimensional road of length n. Along this road,
+# street lights are installed at every point. Each light has a
+# specific range it can cover, which is given by an array named
+# Locations[]. The range for the light at position i extends from
+# the greater of i − Locations[i] or 1, up to the smaller of 
+# i+Locations[i] or n, inclusive. Initially, none of the lights
+# are on. The task is to determine the minimum number of lights
+# that need to be turned on to ensure the entire road is illuminated.
+
+
+
+
+
+
+# n = 3
+# locations[] = {0, 2, 13}
+# ans = 1 as only light at position 2 needs to be activated
