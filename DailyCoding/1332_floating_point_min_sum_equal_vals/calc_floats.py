f212d5911234080285e006cc308fdcb6d14eba4a
stingram <steven.e.ingram@gmail.com>
2025-06-07 17:44:03 -0700

    More efficient num_ways.

---
+++ DailyCoding/1332_floating_point_min_sum_equal_vals/calc_floats.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# given an array of floating point values
+# create list of integers call Y that
+# The rounded sums of both arrays should be equal
+# the sum of the absolute pairwise differences is minimized
--- DailyCoding/1333_num_ways_reach_corner/num_ways.py
+++ DailyCoding/1333_num_ways_reach_corner/num_ways.py
@@ -11,7 +11,7 @@
 #  [1,0,0]]
 
 import numpy as np
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 def _get_options(M: int, N :int, curr_pos: Tuple[int,int], matrix: List[List[int]]):
     options = []
@@ -41,15 +41,38 @@ def num_ways(matrix: List[List[int]]):
 
     return count[0]
 
+
+def _helper(matrix, M, N, curr, memo):
+    if curr[0] == M - 1 and curr[1] == N - 1:
+        memo[curr] = 1
+        return
+
+    if curr not in memo:
+        memo[curr] = 0
+        if curr[0] + 1 < M and matrix[curr[0] + 1][curr[1]] != 1:
+            _helper(matrix, M, N, (curr[0]+1, curr[1]), memo)    
+            memo[curr] += memo[(curr[0]+1,curr[1])]
+        if curr[1] + 1 < N and matrix[curr[0]][curr[1] + 1] != 1:
+            _helper(matrix, M, N, (curr[0], curr[1] + 1), memo)    
+            memo[curr] += memo[(curr[0],curr[1] + 1)]
+
+def num_ways_v2(matrix: List[List[int]]):
+    M = len(matrix)
+    N = len(matrix[0])
+    memo: Dict[Tuple[int,int],int] = {}
+    curr=(0,0)
+    _helper(matrix,M, N, curr,memo)
+    return memo[(0,0)]
+
 # tests
-SIZE = 11
+SIZE = 4
 for _ in range(10):
     M = np.random.randint(2,SIZE)
     x = np.random.random_integers(0,1,(M,M))
     x[0][0] = 0
     x[M-1][M-1] = 0
-    print(f'There are {num_ways(x)} num ways to get from start to end for\n{x}.')
+    print(f'There are {num_ways(x)} num ways and {num_ways_v2(x)} v2 to get from start to end for\n{x}.')
 
 
-# Time is O(M x N)
-# Space is O(M + N)
+# Time is O(M x N) -> don't recompute anything
+# Space is O(M X N) -> memo
